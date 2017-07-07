938157f8e99167a4d34371e655cd93433c4c88fe
David Dejori <dejoridavid@gmail.com>
2017-07-07 16:04:52 +0200

    Moved require statement to the top of the file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
 
+require('express-group-routes');
+
 const express = require('express');
 const connection = require("./database/connection");
 const logger = require("./util/logger");
 
 const v1Receiver = require("./endpoint/v1/receiver");
 
-require('express-group-routes');
 const app = express();
 
 connection.connect(function (error) {

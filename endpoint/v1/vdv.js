1ef694c7725cacb6a21b17c882ca47bf05280ce3
David Dejori <dejoridavid@gmail.com>
2017-07-10 12:56:16 +0200

    Improved VDV zip extraction

--- endpoint/v1/vdv.js
+++ endpoint/v1/vdv.js
@@ -1,25 +1,33 @@
 'use strict';
 
-const logger = require('../../util/logger');
 const fs = require("fs");
+const logger = require("../../util/logger");
+const AdmZip = require("adm-zip");
+
+const latestVdvZip = 'vdv/latest.zip';
+const latestExtractedVdvData = 'vdv/latest';
 
 module.exports = {
 
-    upload: function (req, res) {
+    upload: function (req) {
         return new Promise(function (resolve, reject) {
-            fs.writeFile('vdv/latest.zip', req.body, function(err) {
+            fs.writeFile(latestVdvZip, req.body, function (err) {
                 if (err) {
                     return reject(err);
                 }
 
                 logger.debug("Saved zip file containing VDV data");
 
-                fs.writeFile('vdv/' + new Date().toISOString() + '.zip', req.body, function(err) {
+                new AdmZip(latestVdvZip).extractAllTo(latestExtractedVdvData, true);
+                logger.debug("Extracted latest VDV data");
+
+                fs.writeFile('vdv/' + new Date().toISOString() + '.zip', req.body, function (err) {
                     if (err) {
                         return reject(err);
                     }
 
                     logger.debug("Archived zip file containing VDV data");
+
                     resolve()
                 })
             })

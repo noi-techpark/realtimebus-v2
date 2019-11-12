b9e4e6094fb797578c12d7abe395c3908289593d
Daniel Rampanelli <hello@danielrampanelli.com>
2019-11-12 09:32:17 +0100

    Add documentation regarding the deployment and the "v2" API.

---
+++ tools/setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+CURRENT_DIRECTORY=`pwd`
+
+echo "CONFIGURING INSTANCE..."
+
+sudo apt update
+sudo apt install -y nodejs npm default-jre postgresql-10 postgresql-server-dev-10 postgis recode zip wget git
+
+echo "SETTING UP PROJECT..."
+
+npm install
+
+echo "CONFIGURING DATABASE..."
+
+DATABASE_NAME=`node -e "console.log(require('./local-config.js').database.name);"`
+DATABASE_USERNAME=`node -e "console.log(require('./local-config.js').database.username);"`
+DATABASE_PASSWORD=`node -e "console.log(require('./local-config.js').database.password);"`
+
+if ! [[ `sudo -u postgres psql -tc "SELECT 1 FROM pg_user WHERE usename = '${DATABASE_USERNAME}'"` ]]; then
+    sudo -u postgres psql -c "CREATE USER ${DATABASE_USERNAME};"
+fi
+
+sudo -u postgres psql -c "ALTER USER ${DATABASE_USERNAME} WITH SUPERUSER;"
+
+sudo -u postgres psql -c "ALTER USER ${DATABASE_USERNAME} PASSWORD '${DATABASE_PASSWORD}';"
+
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS ${DATABASE_NAME};"
+
+sudo -u postgres psql -c "CREATE DATABASE ${DATABASE_NAME} OWNER ${DATABASE_USERNAME};"
+
+sudo -u postgres psql -d ${DATABASE_NAME} -c "CREATE EXTENSION postgis;"
+
+CONF_LINE="local all ${DATABASE_USERNAME} md5"
+if ! [[ `sudo grep -Fxq "${CONF_LINE}" /etc/postgresql/10/main/pg_hba.conf` ]]; then
+    DEFAULT_LINE="local   all             postgres                                peer"
+    sudo sed -i "s/${DEFAULT_LINE}/${DEFAULT_LINE}\n\n${CONF_LINE}/g" /etc/postgresql/10/main/pg_hba.conf
+    sudo service postgresql restart
+fi
+
+PGPASSWORD=${DATABASE_PASSWORD} psql -d ${DATABASE_NAME} -U ${DATABASE_USERNAME} < ./database/schema/data.sql
+PGPASSWORD=${DATABASE_PASSWORD} psql -d ${DATABASE_NAME} -U ${DATABASE_USERNAME} < ./database/schema/beacons.sql
+PGPASSWORD=${DATABASE_PASSWORD} psql -d ${DATABASE_NAME} -U ${DATABASE_USERNAME} < ./database/schema/changelog-v1.sql
+
+echo "SETTING UP SERVICE..."
+
+if [[ -f /etc/systemd/system/realtimebus.service ]]; then
+    sudo rm -f /etc/systemd/system/realtimebus.service
+fi
+
+sudo touch /etc/systemd/system/realtimebus.service
+
+cat << EOF | sudo tee -a /etc/systemd/system/realtimebus.service
+[Unit]
+Description="Realtimebus Server"
+
+[Service]
+ExecStart=/usr/bin/node app.js serve
+WorkingDirectory=${CURRENT_DIRECTORY}
+Restart=always
+RestartSec=10
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=realtimebus
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable realtimebus.service
--- tools/update-vdv.sh
+++ tools/update-vdv.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-USERNAME=${1}
-PASSWORD=${2}
-
-echo "IMPORTING CURRENT VDV..."
+BASE_URL=`node -e "console.log(require('./local-config.js').application.baseUrl);"`
+VDV_USERNAME=`node -e "console.log(require('./local-config.js').vdv.username);"`
+VDV_PASSWORD=`node -e "console.log(require('./local-config.js').vdv.password);"`
 
 rm -f ./latest.zip*
 
 wget http://realtimetest.opensasa.info/vdv/list/latest.zip
 
-curl --header "Content-Type:application/octet-stream" --data-binary @./latest.zip --user ${USERNAME}:${PASSWORD} -X POST http://$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/vdv/import
+curl --header "Content-Type:application/octet-stream" --data-binary @./latest.zip --user ${VDV_USERNAME}:${VDV_PASSWORD} -X POST ${BASE_URL}/vdv/import
 
 rm -f ./latest.zip*

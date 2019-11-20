9788d89401053ddca7bc46df18cc86eadbf8409c
bertolla <p.bertolla@noi.bz.it>
2019-11-20 14:19:55 +0100

    CI pipeline + docker config

---
+++ Jenkinsfile-CI.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    stages {
+        stage('Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'npm run test'
+            }
+        }
+    }
+}

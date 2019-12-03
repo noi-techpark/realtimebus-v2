2783394730be5dcc386501c3e7cd0af5d76b14ab
bertolla <p.bertolla@noi.bz.it>
2019-12-03 11:57:40 +0100

    switch agent

--- Jenkinsfile-Prod.groovy
+++ Jenkinsfile-Prod.groovy
@@ -7,11 +7,17 @@ pipeline {
     }
 
     stages {
+        stage('Configure') {
+            steps {
+                sh 'cat ${CONFIG_FILE} > local-config.js'
+            }
+        }
         stage('Deploy') {
             steps {
+                sh 'scp  local-config.js ${PRODUCTION_SERVER}:realtimebus-v2'
                 sh '''
                     ssh ${PRODUCTION_SERVER} \
-                       "cd realtimebus-v2/ && git fetch --all --prune && git reset --hard origin/master && git pull && npm install && cat ${CONFIG_FILE} > local-config.js"
+                       "cd realtimebus-v2/ && git fetch --all --prune && git reset --hard origin/master && git pull && npm install"
                   '''
             }
         }

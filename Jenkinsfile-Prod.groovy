63a36dfb6ed3b8204761031774bb8f3e76b779c8
bertolla <p.bertolla@noi.bz.it>
2020-02-24 10:08:34 +0100

    push to folder / not file

--- Jenkinsfile-Prod.groovy
+++ Jenkinsfile-Prod.groovy
@@ -15,7 +15,7 @@ pipeline {
         stage('Deploy') {
             steps {
                 sh 'ssh ${PRODUCTION_SERVER} [ -d foo ] || mkdir realtimebus-v2'
-                sh 'scp  local-config.js ${PRODUCTION_SERVER}:realtimebus-v2'
+                sh 'scp  local-config.js ${PRODUCTION_SERVER}:realtimebus-v2/local-config.js'
                 sh '''
                     ssh ${PRODUCTION_SERVER} \
                        "cd realtimebus-v2/ && git fetch --all --prune && git reset --hard origin/master && git pull && npm install && sudo systemctl restart realtimebus.service"
